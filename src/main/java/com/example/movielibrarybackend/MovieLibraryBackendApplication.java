361325b1ff052e8a366b78a117ae9d50b1b0c7e9
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-11-08 10:05:42 -0600

    Initial commit.

---
+++ src/main/java/com/example/movielibrarybackend/MovieLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.movielibrarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movielibrarybackend/MovieLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movielibrarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
